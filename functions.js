5a908bd1815d6d25c2c56c1cf3841e6e1f48b717
Njoyable <73nguyen73@gmail.com>
2022-10-18 09:25:07 +0200

    added files

---
+++ functions.js
@@ -0,0 +1,11 @@
+function timeAdd(start1,start2,end1,end2) {
+    var hoursStart = start1*60;
+    var hoursEnd = end1*60;
+    var finalH
+    var finalMin
+
+    while(hoursStart>59) {
+        finalH++;
+        hoursStart -= 60;
+    }
+}
