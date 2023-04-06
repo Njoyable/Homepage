4c594d1c552993cf7fc6718fde145630e7ff695f
nguyen <73nguyen73@gmail.com>
2023-04-06 19:03:36 +0200

    changes

--- restaurants_scripts.js
+++ restaurants_scripts.js
@@ -12,3 +12,24 @@ function readCSV() {
   };
   reader.readAsText(file);
 }
+
+function readCSV(file) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				var contents = xhr.responseText;
+				var lines = contents.split("\n");
+				var output = "";
+				for (var i = 0; i < lines.length; i++) {
+					output += "<li>" + lines[i] + "</li>";
+				}
+				document.getElementById("list").innerHTML = output;
+			} else {
+				console.error(xhr.statusText);
+			}
+		}
+	};
+	xhr.open("GET", file, true);
+	xhr.send();
+}
