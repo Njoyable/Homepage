5b0d4109c75a027edfbec89201b18542e6d88964
nguyen <73nguyen73@gmail.com>
2023-04-06 19:00:16 +0200

    added restaurants list

--- restaurants_scripts.js
+++ restaurants_scripts.js
@@ -1,14 +1,14 @@
-const listContainer = document.getElementById('list-container');
-
-fetch('ha_spots.csv')
-  .then(response => response.text())
-  .then(data => {
-    const rows = data.split('\n');
-    listContainer.innerHTML = '';
-    rows.forEach(row => {
-      const li = document.createElement('li');
-      li.textContent = row.trim();
-      listContainer.appendChild(li);
-    });
-  })
-  .catch(error => console.error(error));
+function readCSV() {
+  var file = "ha_spots.csv"; // Replace with the name of your CSV file
+  var reader = new FileReader();
+  reader.onload = function(e) {
+      var contents = e.target.result;
+      var lines = contents.split("\n");
+      var output = "";
+      for (var i = 0; i < lines.length; i++) {
+          output += "<li>" + lines[i] + "</li>";
+      }
+      document.getElementById("list").innerHTML = output;
+  };
+  reader.readAsText(file);
+}
