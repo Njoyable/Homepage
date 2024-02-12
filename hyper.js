8ab737647aa30d0e3eb3eb8fe4fed75343c37712
Nguyen Khanh Anh N <73nguyen73@gmail.com>
2024-02-12 17:47:19 +0100

    added math

---
+++ hyper.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var menge = document.getElementById("menge");
+    var mengeS = document.getElementById("mengeS");
+    var pulled = document.getElementById("pulled");
+    var special = document.getElementById("special");
+
+    var result = document.getElementById("result");
+   
+    document.querySelector("form").addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        var mengeValue = parseFloat(menge.value);
+        var mengeSValue = parseFloat(mengeS.value);
+        var pulledValue = parseFloat(pulled.value);
+        var special = parseFloat(special.value);
+
+        var prob = calcBinomialCoe((mengeS, special) * calcBinomialCoe((menge-mengeS), (pulled-special))) / calcBinomialCoe(menge, pulled)
+
+        result.textContent = "Wahrscheinlichkeit: " + prob;
+
+    }) 
+});
+
+function calcBinomialCoe(n, k) {
+    function factorial(num) {
+        if (num === 0 || num === 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+
+    if (k < 0 || k > n) {
+        return 0;        
+    } else {
+        return factorial(n) / (factorial(k) * factorial(n-k));
+    }
+};
